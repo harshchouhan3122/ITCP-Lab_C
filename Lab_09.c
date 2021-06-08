adc88706925387ceb7e5666d2bb210747ea19e85
Harsh <harshchouhan3122@gmail.com>
2021-06-08 20:24:17 +0530

    Lab_C

--- Lab_09.c
+++ Lab_09.c
@@ -2,9 +2,9 @@
 /*
 Define a structure called cricket that will describe the following information:
 player name,team name, batting average.
-Using cricket,declare an array player with 5 elements
+Using cricket,declare an array player with input (no.of players) elements
 and write a program to read the information about all
-the 5 players and print ateam-wise list containing
+the no. of  players (input) and print a team-wise list containing
 names of player with their batting average.
 */
 #include <stdio.h>
@@ -20,12 +20,15 @@ struct cricket
 
 int main()
 {
-    struct cricket s[3],temp;
-    int i,j,n=3;
+    
+    struct cricket s[20],temp;
+    int i,j,n;
     float batavg;
+    printf("Total Number of players:\n");
+    scanf("%d",&n);
     for(i=0;i<n;i++)
     {   
-        printf("Enter Details :\n");
+        printf("Enter Details in the following form :\n1.Player Name\n2.Team Name\n3.Batting Average\n");
         scanf("%s %s %f",&s[i].pname,&s[i].tname,&s[i].batavg);
         //s[i].bavg=p;
     }
@@ -38,11 +41,11 @@ int main()
             {
                 temp=s[j-1];
                 s[j-1]=s[j];
-                s[j]=t;
+                s[j]=temp;
             }
         }
     }
-
+    printf("Player Name         Team Name           Batting Avg.\n");
     for(i=0;i<n;i++)
     {
         printf("%-20s %-20s %.2f\n",s[i].pname,s[i].tname,s[i].batavg);
